5601645dfe53ad7eae0b8172ff720aeeacd4e407
Evan Mckenzie <615evan@gmail.com>
2022-08-10 23:45:45 -0400

    SwitchExercise

---
+++ SwitchExercise/Methods.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SwitchExercise;
+
+namespace SwitchExercise
+{
+    internal class Methods
+    {
+        public static void Winner()
+        {
+
+            Random r = new Random();
+            int favNumber = r.Next(1, 100);
+            var guess = int.Parse(Console.ReadLine());
+
+            if (guess > favNumber)
+            {
+                Console.WriteLine($"Your guess of {guess} is too high!\n" +
+                    $"You Lose!");
+
+
+            }
+            else if (guess < favNumber)
+            {
+                Console.WriteLine($"Your guess of {guess} is too low \n" +
+                    $"You Lose!");
+
+            }
+            else 
+            {
+                Console.WriteLine($"Your guess of {guess} is correct! \n" +
+                    $"WINNER!");
+                
+            }
+        }
+
+        public static void SchoolSubjects()
+        {
+            Console.WriteLine("Hey! What is your favorite subject in school!");
+            string favSubject = Console.ReadLine();
+            favSubject.ToLower();
+
+            switch(favSubject)
+            {
+                case "math":
+                    Console.WriteLine("Math is essential! keep up the good work!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is the secret to life!");
+                    break;
+                case "english":
+                    Console.WriteLine("Why do we have to take this for so long?");
+                    break;
+                case "art":
+                    Console.WriteLine("My momma always told me i was artistic!");
+                    break;
+                case "history":
+                    Console.WriteLine("Remember kids, History always repeats itself!");
+                    break;
+                    default:
+                    break;
+
+                    Console.WriteLine("Is that REALLY your favorite subject? \n" +
+                        "Mine too!");
+            }
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+        
+    
+
+
+    }
+
+}
---
+++ SwitchExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿//BEGIN EXERCISE 1
+using SwitchExercise;
+Console.WriteLine("HEY LETS PL.. sorry \n" +
+    "Hey lets play a game \n" +
+    "guess my favorite number between 1 - 100!");
+Methods.Winner();
+//Begin exercise 2
+Methods.SchoolSubjects();
+
+
+
+
+
